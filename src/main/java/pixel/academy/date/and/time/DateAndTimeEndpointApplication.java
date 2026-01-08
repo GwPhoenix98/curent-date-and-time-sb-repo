33da72ce354578293de1b6e8bba022e8123e3397
Ion Schimbator <ionschimbator98@gmail.com>
2026-01-08 17:05:26 +0200

    Initial commit

---
+++ src/main/java/pixel/academy/date/and/time/DateAndTimeEndpointApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.date.and.time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateAndTimeEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateAndTimeEndpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pixel/academy/date/and/time/DateAndTimeEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.date.and.time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateAndTimeEndpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
